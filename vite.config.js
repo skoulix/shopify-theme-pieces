dd9db5b241da4f05a0a845d128420ab5125b6552
Skoulix <evan@seapixel.com>
2026-01-28 17:21:54 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig(() => ({
             '@swup/head-plugin',
             '@swup/js-plugin',
             '@swup/preload-plugin',
-            '@swup/scripts-plugin'
+            '@swup/scripts-plugin',
           ],
         },
       },
